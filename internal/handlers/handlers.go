81df62663ac89ef64251427523f313e74fd4db8f
DushesL <dushes@avlimon.ru>
2025-08-03 16:14:17 +0300

    firstit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,56 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func HtmlHandler(res http.ResponseWriter, req *http.Request) {
+	http.ServeFile(res, req, "../index.html")
+}
+
+func UploadHandler(res http.ResponseWriter, req *http.Request) {
+	req.ParseMultipartForm(32 << 20)
+
+	file, header, err := req.FormFile("myFile")
+	if err != nil {
+		http.Error(res, "error while getting file", http.StatusInternalServerError)
+		return
+	}
+
+	defer file.Close()
+
+	content, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(res, "error while reading file", http.StatusInternalServerError)
+		return
+	}
+
+	converted, err := service.Convert(string(content))
+	if err != nil {
+		http.Error(res, "error while converting", http.StatusInternalServerError)
+		return
+	}
+
+	fileName := time.Now().UTC().Format("20060102150405") + filepath.Ext(header.Filename)
+	newFile, err := os.Create(fileName)
+	if err != nil {
+		http.Error(res, "error while creating file", http.StatusInternalServerError)
+		return
+	}
+	defer newFile.Close()
+
+	_, err = newFile.WriteString(converted)
+	if err != nil {
+		http.Error(res, "error while writing in file", http.StatusInternalServerError)
+		return
+	}
+
+	res.WriteHeader(http.StatusOK)
+	res.Write([]byte(converted))
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,47 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+)
+
+// Структура сервера
+type Server struct {
+	logger *log.Logger
+	server *http.Server
+}
+
+// Функция создания сервера
+func NewServer(logger *log.Logger) *Server {
+	// Создаем новый HTTP-роутер
+	router := http.NewServeMux()
+
+	// Регистрируем хендлеры
+	router.HandleFunc("/", handlers.HtmlHandler)
+	router.HandleFunc("/upload", handlers.UploadHandler)
+
+	// Создаем экземпляр http.Server
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	return &Server{
+		logger: logger,
+		server: server,
+	}
+}
+
+// Метод для запуска сервера
+func (s *Server) Start() error {
+	return s.server.ListenAndServe()
+}
+
+// Метод для остановки сервера
